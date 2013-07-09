c5567e6e73aadc516fc612fb9d991aec91cabf7f
djiternia <dhanjayp@iternia.com>
2013-07-09 18:00:12 +0530

    upload

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@
  * Time: 12:08 PM
  * To change this template use File | Settings | File Templates.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 // Handler for .ready() called.
-    $('#submit').click(function(){
-       // alert("clicked");
+    $('#submit').click(function () {
+        // alert("clicked");
 
         var person = new Object();
         person.firstName = $('#firstName').val();
@@ -18,25 +18,25 @@ $(document).ready(function() {
 
         var jsonPerson = JSON.stringify(person);
 
-       // alert(jsonPerson);
+        // alert(jsonPerson);
         console.log(jsonPerson);
 
         $.ajax({
-            url: "http://localhost:8080/wowmedalv1/api/appTest",
-            method:'POST',
-            data:jsonPerson,
-            dataType:'json',
-            contentType:"application/json"
+            url: "http://server1.iternia.com:9090/iterniaapp/api/person",
+            method: 'POST',
+            data: jsonPerson,
+            dataType: 'json',
+            contentType: "application/json"
 
 
-        }).done(function(data) {
-               // alert("send"+data);
+        }).done(function (data) {
+                // alert("send"+data);
                 console.log(data);
                 //var responseObj = $.parseJSON(data);
                 //alert(data);
                 $('#showFirstName').html("First Name :- " + data.FirstName);
                 $('#showLastName').html("Last Name :- " + data.LastName);
-        });
+            });
 
     });
 });
