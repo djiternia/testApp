e528c1190b3a8b224bcf328bf085c5d003905618
djiternia <dhanjayp@iternia.com>
2013-07-10 12:24:16 +0530

    upload

--- js/main.js
+++ js/main.js
@@ -8,36 +8,37 @@
 $(document).ready(function () {
 // Handler for .ready() called.
     alert("jquery ");
-    $('#submit').tap(function () {
-        alert("clicked");
+    /*    $('#submit').tap(function () {
+     alert("clicked");
 
-        var person = new Object();
-        person.firstName = $('#firstName').val();
-        person.lastName = $('#lastName').val();
+     var person = new Object();
+     person.firstName = $('#firstName').val();
+     person.lastName = $('#lastName').val();
 
-        //alert(person);
+     //alert(person);
 
-        var jsonPerson = JSON.stringify(person);
+     var jsonPerson = JSON.stringify(person);
 
-        // alert(jsonPerson);
-        console.log(jsonPerson);
+     // alert(jsonPerson);
+     console.log(jsonPerson);
 
-        $.ajax({
-            url: "http://server1.iternia.com:9090/iterniaapp/api/addperson",
-            method: 'POST',
-            data: jsonPerson,
-            dataType: 'json',
-            contentType: "application/json"
+     $.ajax({
+     url: "http://server1.iternia.com:9090/iterniaapp/api/addperson",
+     method: 'POST',
+     data: jsonPerson,
+     dataType: 'json',
+     contentType: "application/json"
 
 
-        }).done(function (data) {
-                // alert("send"+data);
-                console.log(data);
-                //var responseObj = $.parseJSON(data);
-                //alert(data);
-                $('#showFirstName').html("First Name :- " + data.firstName);
-                $('#showLastName').html("Last Name :- " + data.lastName);
-            });
+     }).done(function (data) {
+     // alert("send"+data);
+     console.log(data);
+     //var responseObj = $.parseJSON(data);
+     //alert(data);
+     $('#showFirstName').html("First Name :- " + data.firstName);
+     $('#showLastName').html("Last Name :- " + data.lastName);
+     });
 
-    });
+     });
+     */
 });
